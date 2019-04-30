b22941995c3f0caad3b23706d70476698231a9b9
magic-akari <hufan.akarin@gmail.com>
2019-04-30 21:17:39 +0800

    add: scrollIntoView

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { IAnimationOptions } from "./common.js";
 import { polyfill as elementScrollPolyfill } from "./Element.scroll.js";
 import { polyfill as elementScrollByPolyfill } from "./Element.scrollBy.js";
+import { polyfill as elementScrollIntoViewPolyfill } from "./Element.scrollIntoView.js";
 import { polyfill as elementScrollToPolyfill } from "./Element.scrollTo.js";
 import { polyfill as windowScrollPolyfill } from "./Window.scroll.js";
 import { polyfill as windowScrollByPolyfill } from "./Window.scrollBy.js";
 import { polyfill as windowScrollToPolyfill } from "./Window.scrollTo.js";
 
-export const seamless = (options: IAnimationOptions) => {
+export const polyfill = (options: IAnimationOptions) => {
     if ("scrollBehavior" in document.documentElement.style) {
         return;
     }
@@ -18,12 +19,14 @@ export const seamless = (options: IAnimationOptions) => {
     elementScrollPolyfill(options);
     elementScrollToPolyfill(options);
     elementScrollByPolyfill(options);
+    elementScrollIntoViewPolyfill(options);
 };
 
 export { elementScroll } from "./Element.scroll.js";
 export { elementScrollBy } from "./Element.scrollBy.js";
+export { elementScrollIntoView } from "./Element.scrollIntoView.js";
 export { elementScrollTo } from "./Element.scrollTo.js";
 export { windowScroll } from "./Window.scroll.js";
 export { windowScrollBy } from "./Window.scrollBy.js";
 export { windowScrollTo } from "./Window.scrollTo.js";
-export { seamless as polyfill };
+export { polyfill as seamless };
