68bb19a5e1ad5734e7d04555bd1b0d38dd39cd6a
magic-akari <akari.ccino@gmail.com>
2021-09-14 01:18:39 +0800

    chore: tweak code

--- src/common.ts
+++ src/common.ts
@@ -2,34 +2,11 @@ export const checkBehavior = (behavior?: string): behavior is undefined | Scroll
     return behavior === undefined || behavior === "auto" || behavior === "instant" || behavior === "smooth";
 };
 
-export function elementScrollXY(this: Element, x: number, y: number): void {
-    this.scrollLeft = x;
-    this.scrollTop = y;
-}
-
 export const failedExecute = (method: string, object: string, reason = "cannot convert to dictionary."): string =>
     `Failed to execute '${method}' on '${object}': ${reason}`;
 
 export const failedExecuteInvalidEnumValue = (method: string, object: string, value: string): string =>
     failedExecute(method, object, `The provided value '${value}' is not a valid enum value of type ScrollBehavior.`);
-
-interface GetOriginalMethod {
-    <K extends keyof Element>(proto: Element, method: K, fallback?: unknown): Element[K];
-    <K extends keyof Window>(proto: Window, method: K, fallback?: unknown): Window[K];
-}
-
-/* eslint-disable */
-export const getOriginalMethod: GetOriginalMethod = (proto: any, method: string, fallback?: unknown) => {
-    const backup = `__seamless__$$${method}$$__backup__`;
-    proto[backup] ||= proto[method] ||= fallback;
-
-    if (proto[backup]?.__isPolyfill) {
-        throw new Error("unexpected_method");
-    }
-
-    return proto[backup];
-};
-/* eslint-enable */
 
 export const isObject = (value: unknown): boolean => {
     const type = typeof value;
